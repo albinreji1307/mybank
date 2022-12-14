b2dcd39a31084d9a2e1ae48b84b96a7ba8a34681
Albin Reji <albinreji1307@gmail.com>
2022-12-14 18:22:31 +0530

    HI

--- src/Firstpage.jsx
+++ src/Firstpage.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
                 }}
               >
                 <Button style={{ color: "black"  }} >
-                ASDFGHJKL;
+                ASDFGHJKLASDFGHJK
 
                 </Button>
               </NavLink>
