2721b6dfa16fc005abb033e7c7c702ab9904a6f2
Albin Reji <albinreji1307@gmail.com>
2022-12-14 17:02:31 +0530

    locally saved

--- src/Firstpage.jsx
+++ src/Firstpage.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
                 }}
               >
                 <Button style={{ color: "black"  }} >
-                User Dashboard
+                Dashboard
                 </Button>
               </NavLink>
               
